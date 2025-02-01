1b8ec4111df33e16fc95911a67b075ffffebbcb8
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-02-02 01:59:03 +0530

    Update 03_Time Conversion.cpp

--- 03_Time Conversion.cpp
+++ 03_Time Conversion.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    string s ;
+    cin >> s ;
+    int h = stoi(s.substr(0, 2));
+    string m = s.substr(3, 2);
+    string sec = s.substr(6, 2);
+    string period = s.substr(8, 2); 
+    if(period == "AM"){
+        if(h == 12){
+            h = 0 ;
+        }
+    }
+    else if(period == "PM"){
+        if(h != 12){
+            h +=12 ;
+        }
+    }
+    string timein24 = (h < 10 ? "0" : "") + to_string(h) + ":" + m + ":" + sec ;
+    cout << timein24 ;
+    exit(0);
+}
