e5dfd3bbcc626b97ef9e8a236354dfa007ced471
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-02-02 01:58:09 +0530

    Update 02_Mini-Max Sum.cpp

--- 02_Mini-Max Sum.cpp
+++ 02_Mini-Max Sum.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    long arr[5] ;
+    long min , max , sum = 0 ;
+    for(int i = 0 ; i < 5 ; i++){
+        cin >> arr[i] ;
+        sum += arr[i] ;
+    }
+    min = arr[1] ;
+    max = min ;
+    
+    for(int i = 0 ; i < 5 ; i++){
+        if(arr[i] < min){
+            min = arr[i] ;
+        }
+        if(arr[i] > max){
+            max = arr[i] ;
+        }
+    }
+    
+    cout << sum - max << " " << sum - min ; 
+    
+    exit(0);
+}
