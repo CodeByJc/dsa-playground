4da96b9666d2d4c8e47f990b3061497adf29aaa0
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-02-02 12:26:45 +0530

    Create Breaking the Records.cpp

---
+++ Breaking the Records.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int game ;
+    cin >> game ;
+    int score[game] ;
+    int min , max , c_min = 0 , c_max = 0 ;
+    
+    for(int i = 0 ; i < game ; i++){
+        cin >> score[i];
+    }
+    min = max = score[0] ;
+    
+    for(int i = 1 ; i < game ; i++){
+        if(score[i] < min){
+            min = score[i] ;
+            c_min++;
+        }
+        if(score[i] > max){
+            max = score[i] ;
+            c_max++;
+        }
+    }
+    
+    cout << c_max << " " << c_min ;
+    
+    return 0;
+}
