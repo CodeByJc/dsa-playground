6abfbc8152671ab04c920aa445bb7e232df3b23b
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-07-21 19:39:38 +0530

    Create math_practise.cpp

---
+++ Strivers_DSA/math_practise.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n) {
+    if (n == 0) return 1;
+    int counter = 0;
+    while (n > 0) {
+        n /= 10;
+        counter++;
+    }
+    return counter;
+}
+
+int reverseNumber(int n) {
+    int reversed = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        reversed = (reversed * 10) + digit;
+        n /= 10;
+    }
+    return reversed;
+}
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Number of Digits : " << countDigits(n) << endl;
+    cout << "Reversed Number  : " << reverseNumber(n) << endl;
+    cout << "Prime or Not     : " << (isPrime(n) ? "Yes" : "No") << endl;
+    return 0;
+
+    
+
+}
