af0c3a20cea8eedd78b1581db57a309d20892247
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-08-03 10:59:23 +0530

    Update sorting_practise.cpp

--- Strivers_DSA/sorting_practise.cpp
+++ Strivers_DSA/sorting_practise.cpp
@@ -1,68 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool sorted(int arr[], int size){ 
-    bool isSsorted = true ;
-    int i = 0 ;
-    while(i < size - 1){
-        if(arr[i] > arr[i+ 1]){
-            isSsorted = false ;
+
+// Function to check if an array is sorted
+bool sorted(int arr[], int size) {
+    bool isSorted = true;
+    int i = 0;
+    while (i < size - 1) {
+        if (arr[i] > arr[i + 1]) {
+            isSorted = false;
         }
-        if(isSsorted == false) return isSsorted ;
-        i++; 
+        if (!isSorted) return isSorted;
+        i++;
     }
-    return isSsorted ;
+    return isSorted;
 }
 
-// void selection_sort(int arr[],int size){
-//     int min_index;
-//     for(int i = 0 ; i < size - 1; i++){
-//         min_index = i ;
-//         for(int j = i + 1 ; j < size ; j++){
-//             if(arr[j] < arr[min_index]){
-//                 min_index = j ;
-//             }
-//         }
-//         swap(arr[i],arr[min_index]);
-//     }
-    
-//     for(int i = 0 ; i < size ;i++){
-//         cout << arr[i] << " " ;
-//     }
-// }
+/* Selection Sort
+void selection_sort(int arr[], int size) {
+    int min_index;
+    for (int i = 0; i < size - 1; i++) {
+        min_index = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(arr[i], arr[min_index]);
+    }
 
-// void bubble_sort(int arr[],int size){
-//     bool swapped = false ;
-    
-//     for(int i = size ; i > 0; i--){
-//         for(int j = 0 ; j < i ; j++){
-//             if(arr[j] > arr[j + 1]){
-//                 swap(arr[j],arr[j + 1]);
-//                 swapped = true ;
-//             }
-//         }
-//         if(swapped == false){
-//             break ;
-//         }
-//     }
-    
-//     for(int i = 0 ; i < size ;i++){
-//         cout << arr[i] << " " ;
-//     }
-// }
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+*/
 
-// void insertion_sort(int arr[],int size){
-//     for(int i = 1 ; i < size ; i++){
-//         int j = i ;
-//         while(j > 0 && arr[j - 1] > arr[j]){ 
-//             swap(arr[j],arr[j - 1]);
-//             j--;
-//         }
-//     }
-//     for(int i = 0 ; i < size ;i++){
-//         cout << arr[i] << " " ;
-//     }
-// }
+/* Bubble Sort
+void bubble_sort(int arr[], int size) {
+    bool swapped = false;
 
+    for (int i = size; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+*/
+
+/* Insertion Sort
+void insertion_sort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+*/
+
+// Merge function for Merge Sort
 void merge(int arr[], int low, int mid, int high) {
     vector<int> temp;
     int left = low, right = mid + 1;
@@ -81,18 +88,20 @@ void merge(int arr[], int low, int mid, int high) {
         temp.push_back(arr[left]);
         left++;
     }
+
     while (right <= high) {
         temp.push_back(arr[right]);
         right++;
     }
 
     for (int i = 0; i < (int)temp.size(); i++) {
-        arr[low + i] = temp[i];  // Copy back to original array
+        arr[low + i] = temp[i]; // Copy back to original array
     }
 }
 
+// Merge Sort function
 void merge_sort(int arr[], int low, int high) {
-    if (low >= high) return;  // Base case
+    if (low >= high) return; // Base case
 
     int mid = (low + high) / 2;
     merge_sort(arr, low, mid);
@@ -100,28 +109,24 @@ void merge_sort(int arr[], int low, int high) {
     merge(arr, low, mid, high);
 }
 
-int main()
-{
+int main() {
     int size = 10;
-    int arr[size] = {5,4,2,7,1,15,3,10,8,6};
-    
-    
-    // selection_sort(arr,size);
-    // cout << endl ;
-    
-    // bubble_sort(arr,size);
-    // cout << endl ;
-    
-    
-    // insertion_sort(arr,size);
-    // cout << endl ;
+    int arr[size] = {5, 4, 2, 7, 1, 15, 3, 10, 8, 6};
 
-    // merge_sort(arr,0,size - 1);
+    // selection_sort(arr, size);
+    // bubble_sort(arr, size);
+    // insertion_sort(arr, size);
 
-    // for(int i = 0 ; i < size ;i++){
-    //     cout << arr[i] << " " ;
+    // merge_sort(arr, 0, size - 1);
+
+    // for (int i = 0; i < size; i++) {
+    //     cout << arr[i] << " ";
     // }
-    
-    if(sorted(arr,size)) cout << "Sorted" ; else cout << "Not Sorted";
+
+    if (sorted(arr, size))
+        cout << "Sorted";
+    else
+        cout << "Not Sorted";
+
     return 0;
 }
