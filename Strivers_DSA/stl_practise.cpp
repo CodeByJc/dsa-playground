e777f4233507ee410395a8ff7885250267739865
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-08-03 10:55:49 +0530

    Update stl_practise.cpp

--- Strivers_DSA/stl_practise.cpp
+++ Strivers_DSA/stl_practise.cpp
@@ -1,54 +1,53 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
+int main() {
+    vector<int> jc(0, 3);
 
-
-int main()
-{
-    vector<int> jc(0,3);
-    
     jc.push_back(1);
     jc.emplace_back(2);
     jc.emplace_back(23);
     jc.emplace_back(24);
+
     auto start = jc.begin();
     auto end = jc.end();
-    
-    // cout << *(start) << " " << *(--end) ;
-    
+
+    // Uncomment to print first and last elements
+    // cout << *(start) << " " << *(--end);
+
+    // Uncomment to print last element
     // cout << jc.back();
-    
-    // for(start ; start != end ; start++){
+
+    // Uncomment to iterate and print elements
+    // for (start; start != end; start++) {
     //     cout << *(start) << " ";
     // }
-    // cout << endl ;
-    
-    jc.insert(jc.begin(),300);
-    jc.insert(jc.end(),200);
-    jc.insert(jc.begin() + 1,2, 7);
-    
-    vector<int> jc1(2,50);
-    jc.insert(jc.begin(), jc1.begin(),jc1.end());
+    // cout << endl;
 
-    
-    // for(auto  start1 : jc1){
+    // Insert elements into jc
+    jc.insert(jc.begin(), 300);
+    jc.insert(jc.end(), 200);
+    jc.insert(jc.begin() + 1, 2, 7);
+
+    vector<int> jc1(2, 50);
+    jc.insert(jc.begin(), jc1.begin(), jc1.end());
+
+    // Uncomment to print elements of jc1
+    // for (auto start1 : jc1) {
     //     cout << start1 << " ";
     // }
-    
-    cout << jc.size() << " " << jc1.size() ;
-    
-    cout << jc.empty() ;
-    
+
+    cout << jc.size() << " " << jc1.size();
+    cout << jc.empty();
+
+    // Uncomment to clear jc
     // jc.clear();
-    
+
     jc.swap(jc1);
-    
-     for(auto  start1 : jc1){
+
+    for (auto start1 : jc1) {
         cout << start1 << " ";
     }
-    
-    
-    
-    
+
     return 0;
 }
