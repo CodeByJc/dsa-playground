f38ce77566e65505acc2cc056498268195f38a24
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-08-03 11:15:53 +0530

    Update recursion1.cpp

--- Strivers_DSA/recursion1.cpp
+++ Strivers_DSA/recursion1.cpp
@@ -22,7 +22,7 @@ void printNumbersDecreasing(int i, int n) {
     printNumbersDecreasing(i, n - 1);
 }
 
-// Print sum of first n natural numbers
+// Print sum of first n natural numbers (formula-based)
 void printSumOfN(int n) {
     int sum = n * (n + 1) / 2;
     cout << "The sum of the first " << n << " numbers is: " << sum << endl;
@@ -38,45 +38,4 @@ int factorial(int n) {
 void reverseArray(int arr[], int start, int end) {
     if (start < end) {
         swap(arr[start], arr[end]);
-        reverseArray(arr, start + 1, end - 1);
-    }
-}
-
-// Print array
-void printArray(int arr[], int n) {
-    cout << "The reversed array is: ";
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-    int n = 4;
-
-    cout << "--- Print Name ---" << endl;
-    printName(1, n);
-
-    cout << "\n--- Numbers from 1 to n ---" << endl;
-    printNumbersIncreasing(1, n);
-
-    cout << "\n--- Numbers from n to 1 ---" << endl;
-    printNumbersDecreasing(1, n);
-
-    cout << "\n--- Sum of first n numbers ---" << endl;
-    printSumOfN(n);
-
-    cout << "\n--- Factorial ---" << endl;
-    cout << "Factorial of " << n << " is: " << factorial(n) << endl;
-
-    int arr[n];
-    cout << "\n--- Enter " << n << " elements for the array ---" << endl;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-
-    reverseArray(arr, 0, n - 1);
-    printArray(arr, n);
-
-    return 0;
-}
+        reverseAr
