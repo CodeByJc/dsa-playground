4f856d5cbd328947961f1cc787250cb97313213b
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-08-03 11:13:58 +0530

    Update math_practise.cpp

--- Strivers_DSA/math_practise.cpp
+++ Strivers_DSA/math_practise.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Function to count digits of a number
 int countDigits(int n) {
     if (n == 0) return 1;
     int counter = 0;
@@ -11,6 +12,7 @@ int countDigits(int n) {
     return counter;
 }
 
+// Function to reverse a number
 int reverseNumber(int n) {
     int reversed = 0;
     while (n > 0) {
@@ -21,11 +23,11 @@ int reverseNumber(int n) {
     return reversed;
 }
 
+// Function to check if a number is prime
 bool isPrime(int n) {
     if (n <= 1) return false;
     for (int i = 2; i * i <= n; ++i) {
-        if (n % i == 0)
-            return false;
+        if (n % i == 0) return false;
     }
     return true;
 }
@@ -38,8 +40,6 @@ int main() {
     cout << "Number of Digits : " << countDigits(n) << endl;
     cout << "Reversed Number  : " << reverseNumber(n) << endl;
     cout << "Prime or Not     : " << (isPrime(n) ? "Yes" : "No") << endl;
+
     return 0;
-
-    
-
 }
