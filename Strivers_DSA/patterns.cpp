ebf760a87d5b1967810794558b38d58a2db0091c
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-08-03 11:14:59 +0530

    Update patterns.cpp

--- Strivers_DSA/patterns.cpp
+++ Strivers_DSA/patterns.cpp
@@ -1,218 +1,176 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 
-void pattern1(int n){
-    
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < n ; j++){
-            cout  << "*" ;
-        }
-        cout << endl ;
-    }
-}
-
-void pattern2(int n){
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < i + 1 ; j++){
-            cout  << "*" ;
-        }
-        cout << endl ;
-    }
-}
-
-void pattern3(int n){
-    int num ;
-    for (int i = 0 ; i < n ; i++){
-        num = 1 ;
-        for (int j = 0 ; j < i + 1 ; j++){
-            cout  << num ;
-            num ++ ;
-        }
-        cout << endl ;
-    }
-}
-
-void pattern4(int n){
-    int num = 1 ;
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < i + 1 ; j++){
-            cout  << num ;
-        }
-        num ++ ;
-        cout << endl ;
-    }
-}
-
-void pattern5(int n){
-    for (int i = n ; i > 0 ; i--){
-        for (int j = 0 ; j < i  ; j++){
-            cout  << "*" ;
-        }
-        cout << endl ;
-    }
-}
-
-void pattern6(int n){
-    int num ;
-    for (int i = n ; i > 0 ; i--){
-        num = 1 ;
-        for (int j = 0 ; j < i  ; j++){
-            cout  << num ;
-            num++ ;
-        }
-        cout << endl ;
-    }
-}
-
-void pattern7(int n){
-    for (int i = 0 ; i < n ; i++){
-        
-        //space ;
-        
-        for(int space  = 0 ; space < n - i - 1 ; space++){
-            cout << " ";
-        }
-        
-        //star
-        
-        for(int star  = 0 ; star < 2 * i + 1  ; star++){
+// Pattern 1: Solid square of '*'
+void pattern1(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << "*";
         }
-        
-        //space
-        
-        for(int space  = 0 ; space < n - i - 1 ; space++){
-            cout << " ";
-        }
-        cout << endl ;
+        cout << endl;
     }
 }
 
-void pattern8(int n){
-    for (int i = n ; i > 0 ; i--){
-        
-        //space ;
-        
-        for(int space  = 0 ; space < n - i ; space++){
-            cout << " ";
-        }
-        
-        //star
-        
-        for(int star  = 0 ; star < 2 * i - 1  ; star++){
+// Pattern 2: Right-angled triangle of '*'
+void pattern2(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
             cout << "*";
         }
-        
-        //space
-        
-        for(int space  = 0 ; space < n - i ; space++){
-            cout << " ";
-        }
-        cout << endl ;
+        cout << endl;
     }
 }
 
-void pattern9(int n){
+// Pattern 3: Increasing numbers in each row
+void pattern3(int n) {
+    for (int i = 0; i < n; i++) {
+        int num = 1;
+        for (int j = 0; j <= i; j++) {
+            cout << num;
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 4: Same number repeated in each row
+void pattern4(int n) {
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << num;
+        }
+        num++;
+        cout << endl;
+    }
+}
+
+// Pattern 5: Inverted right-angled triangle of '*'
+void pattern5(int n) {
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 6: Inverted right-angled triangle of numbers
+void pattern6(int n) {
+    for (int i = n; i > 0; i--) {
+        int num = 1;
+        for (int j = 0; j < i; j++) {
+            cout << num;
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+// Pattern 7: Pyramid of '*'
+void pattern7(int n) {
+    for (int i = 0; i < n; i++) {
+        // Spaces
+        for (int space = 0; space < n - i - 1; space++) cout << " ";
+        // Stars
+        for (int star = 0; star < 2 * i + 1; star++) cout << "*";
+        // Spaces (optional, for symmetry)
+        for (int space = 0; space < n - i - 1; space++) cout << " ";
+        cout << endl;
+    }
+}
+
+// Pattern 8: Inverted pyramid of '*'
+void pattern8(int n) {
+    for (int i = n; i > 0; i--) {
+        // Spaces
+        for (int space = 0; space < n - i; space++) cout << " ";
+        // Stars
+        for (int star = 0; star < 2 * i - 1; star++) cout << "*";
+        // Spaces (optional, for symmetry)
+        for (int space = 0; space < n - i; space++) cout << " ";
+        cout << endl;
+    }
+}
+
+// Pattern 9: Diamond (Pattern 7 + Pattern 8)
+void pattern9(int n) {
     pattern7(n);
     pattern8(n);
 }
 
-void pattern10(int n){
-    for(int i = 0 ; i < (n * 2) - 1 ; i++){
-        //star
-        int star = i + 1 ;
-        if(i >= n) star = 2 * n - (i+ 1) ;
-        for(int j = 0 ; j < star ; j++){
-            cout << "*";
-        }
-        cout << endl ;
+// Pattern 10: Hourglass pattern of '*'
+void pattern10(int n) {
+    for (int i = 0; i < (n * 2) - 1; i++) {
+        int star = (i < n) ? i + 1 : (2 * n - i - 1);
+        for (int j = 0; j < star; j++) cout << "*";
+        cout << endl;
     }
 }
 
-void pattern11(int n){
-    int start ;
-    
-    for(int i = 0 ; i < n ; i++){
-        start = 1 ;
-        if(i % 2 == 0){
-            start = 1 ;
+// Pattern 11: Binary triangle
+void pattern11(int n) {
+    for (int i = 0; i < n; i++) {
+        int start = (i % 2 == 0) ? 1 : 0;
+        for (int j = 0; j <= i; j++) {
+            cout << start;
+            start = 1 - start;
         }
-        else{
-            start = 0 ;
-        }
-        
-        for(int j = 0 ; j < i + 1 ; j++){
-            cout  << start ;
-            start = 1 - start ;
-        }
-        cout << endl ;
+        cout << endl;
     }
 }
 
-void pattern14(int n){
-    char a;
-    
-    for( int i = 0 ; i < n ; i++){
-        
-        a = 'A' ;
-        
-        for(int j = 0 ; j < i + 1 ; j++){
-            cout << a ;
+// Pattern 14: Increasing characters starting from 'A'
+void pattern14(int n) {
+    for (int i = 0; i < n; i++) {
+        char a = 'A';
+        for (int j = 0; j <= i; j++) {
+            cout << a;
             a++;
         }
-        cout<< endl ;
+        cout << endl;
     }
 }
 
-void pattern15(int n){
-    char a;
-    
-    for( int i = n ; i > 0 ; i--){
-        
-        a = 'A' ;
-        
-        for(int j = 0 ; j < i  ; j++){
-            cout << a ;
+// Pattern 15: Inverted triangle of characters
+void pattern15(int n) {
+    for (int i = n; i > 0; i--) {
+        char a = 'A';
+        for (int j = 0; j < i; j++) {
+            cout << a;
             a++;
         }
-        cout<< endl ;
+        cout << endl;
     }
 }
 
-void pattern16(int n){
-    char a;
-    a = 'A' ;
-    
-    for( int i = 0 ; i < n ; i++){
-        
-        for(int j = 0 ; j < i + 1  ; j++){
-            cout << a ;
-        }
+// Pattern 16: Repeated characters per row
+void pattern16(int n) {
+    char a = 'A';
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) cout << a;
         a++;
-        cout<< endl ;
+        cout << endl;
     }
 }
 
-void pattern18(int n){
-    char a;
-    a = 'E' ;
-    for( int i = 0 ; i < n ; i++){
-        
-        for(int j = 0 ; j < i + 1  ; j++){
-            cout << a ;
-            if(j < i ) a++;
+// Pattern 18: Character triangle decreasing alphabet
+void pattern18(int n) {
+    char a = 'E';
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << a;
+            if (j < i) a++;
         }
-        a = a - (i + 1);
-        cout<< endl ;
+        a -= (i + 1);
+        cout << endl;
     }
 }
 
-int main()
-{
-    int n ;
-    
-    cin >> n ;
-        
+int main() {
+    int n;
+    cin >> n;
+
     pattern1(n);
     pattern2(n);
     pattern3(n);
@@ -228,5 +186,6 @@ int main()
     pattern15(n);
     pattern16(n);
     pattern18(n);
+
     return 0;
 }
