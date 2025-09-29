61715e9d500863ff3ee6b5c9f08c6ec4ba85ba70
CodeByJc <144438459+CodeByJc@users.noreply.github.com>
2025-09-29 20:33:15 +0530

    Create Angry_Professor.cpp

---
+++ HackerRank_Algorithms/Angry_Professor.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string angryProfessor(int k, vector<int> a) {
+    int onTime = 0;
+    for (int time : a) {
+        if (time <= 0) onTime++;
+    }
+    return (onTime >= k) ? "NO" : "YES";
+}
+
+int main() {
+    int t;
+    cin >> t;   // number of test cases
+    while (t--) {
+        int n, k;
+        cin >> n >> k;   // number of students, threshold
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << angryProfessor(k, a) << endl;
+    }
+    return 0;
+}
